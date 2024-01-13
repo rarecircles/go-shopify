811639f21b4ee6f1e80b4499786fd4b68bf435fe
Julien Cassis <juliencassis@gmail.com>
2024-01-12 21:51:44 -0500

    Inventory management nullable

--- variant.go
+++ variant.go
@@ -56,7 +56,7 @@ type Variant struct {
 	Price                *decimal.Decimal       `json:"price,omitempty"`
 	CompareAtPrice       *decimal.Decimal       `json:"compare_at_price,omitempty"`
 	FulfillmentService   string                 `json:"fulfillment_service,omitempty"`
-	InventoryManagement  string                 `json:"inventory_management,omitempty"`
+	InventoryManagement  *string                `json:"inventory_management,omitempty"`
 	InventoryItemId      int64                  `json:"inventory_item_id,omitempty"`
 	Option1              string                 `json:"option1,omitempty"`
 	Option2              string                 `json:"option2,omitempty"`
